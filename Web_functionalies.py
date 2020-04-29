1615d0fa0cd4014254b4a26e0e2ad7f7c833b5c2
achrefbo98 <46647953+achrefbo98@users.noreply.github.com>
2020-04-30 01:58:32 +0200

    Connection to FireBase Cloud UR

--- Web_functionalies.py
+++ Web_functionalies.py
@@ -1,6 +1,40 @@
 
 
 
-class Web_Api():
+from firebase import firebase
 
+
+
+
+
+class Web_Api():
+    def __init__(self,URL):
+        self.URL=URL
+        # Data base initialisation parameters
+        self.Tirebase=firebase.FirebaseApplication(self.URL,None)
+       
+        
+        
+        
+    def read(self,column_Name,Attribute_Name):
+        # column_Name: name of collection 
+        #  Attribute_Name: columns of DB (UserID,PlateNum,TotalAmoun,EnterCounter)
+        
+        result = self.Tirebase.get('/'+column_Name+'/', Attribute_Name)
+        print(result)
+        
+    def update(self,column_Name, UserId,New_Entercounter):
+        self.Tirebase.put('/'+column_Name+'/','EnterCounter',New_Entercounter)
+    
+        
+        
+      
+        
+
+test=Web_Api('https://web-functionalies-api.firebaseio.com/')
+test.read('subscriber','EnterCounter')
+test.update('subscriber','123456','100')
+test.read('subscriber','EnterCounter')
+    
+    
     

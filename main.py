23085f61847680a858be175cc997326586195e49
root <root@localhost.localdomain>
2020-05-03 05:06:19 -0500

    Bug Fixed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Ai_functionalities import Detection
 from DB_Manipulation import DB
 from Web_functionalies import Web_Api
 from Raspberry_function import Raspberry
+import time
 
 #initialisation
 ai=Detection()
@@ -14,20 +15,20 @@ rasp=Raspberry()
 if __name__ == "__main__":
     if (rasp.sensors_location ):  #if sensor detect the car
         img=rasp.Image_Capture()  # then the camera take image of car
-        ai.main_ai(img)           # then the model of detection plate will be active and croped this img and convert img_cropped to text
+        ai.main_ai(img)           # then the model of detection plate will be activated and will crop the img and convert it from img_cropped to text
         f=open('ocr.txt','r')
         plate_Num=f.read()        #save the plate_num txt in file
         f.close()
         try:
             list_info(db.get_userInfo(plate_Num))  #return the UserID and the EnterCounter of this Plate_Num
             if ((list_info)[1]<-3):  #if EnterCounter<-3
-                print("u can not enter !!! Bitch ")
+                print("u can not enter !!! ")
             else:
-                rasp.Open_Toll() #if EnterCounter>-3  : open the toll
+                rasp.Open_Toll() #if EnterCounter>-3  : open the toll  bug fixed
                 time.sleep(150)
-                db.update(list_info[0],list_info[1]-1) #update of localDatabase       #list_info[0]: UserId 
-                                                                                          
-                Web.update(list_info[0],list_info[1]-1) #Update of web DataBase       #list _info[1]:  EnterCounter 
+                db.update(list_info[0],list_info[1]-1) #update the localDatabase       #list_info[0]: UserId 
+                time.sleep(30)                                                                           
+                Web.update(list_info[0],list_info[1]-1) #Update the web DataBase       #list _info[1]:  EnterCounter 
                 
                 
                 

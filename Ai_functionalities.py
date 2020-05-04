d2b6bf0e3f199a88f2c5bf6bba2b98a8e6185022
Raouf kioua <54064593+Raoufkioua@users.noreply.github.com>
2020-05-04 05:54:17 +0200

    Update Ai_functionalities.py

--- Ai_functionalities.py
+++ Ai_functionalities.py
@@ -1,27 +1,23 @@
 # libraries import
-
-
-
 class Detection():
     def __init__(self):
-        
         pass
 
     def plate_detection(self,img):
         dimention=()
-        return dimention #tuple for positioning where we'll crop the img
+        return dimention            #tuple for positioning where we'll crop the img
 
     def crop(self,img,dimention):
         cropped_img=''
-        return cropped_img
+        return cropped_img          #Crop an image with a specefic dimentions
 
-    def Ocr(self,cropped_img):
+    def Ocr(self,cropped_img):      #Image to text
         ocr_txt=""
         f=open("ocr.txt","w")
         f.write(ocr_txt)
         f.close()
         
-    def main_ai(self,img):
+    def main_ai(self,img):              # Containe the whole process
         dim=self.plate_detection(img)
         img_cropped=self.crop(dim)
         self.Ocr(img_cropped)

981eb1460d81f27f4de52ac9fc708986f4779fd3
Raouf kioua <54064593+Raoufkioua@users.noreply.github.com>
2020-05-04 05:47:55 +0200

    Update Web_functionalies.py

--- Web_functionalies.py
+++ Web_functionalies.py
@@ -1,23 +1,11 @@
-
-
-
 from firebase import firebase
-
-
-
-
 
 class Web_Api():
     def __init__(self,URL):
         self.URL=URL
         # Data base initialisation parameters
         self.Tirebase=firebase.FirebaseApplication(self.URL,None)
-      
-        
-       
-        
-        
-        
+    
     def read(self,UserID,Attribute_Name):
         # column_Name: name of collection 
         #  Attribute_Name: columns of DB (UserID,PlateNum,TotalAmoun,EnterCounter)
@@ -26,13 +14,14 @@ class Web_Api():
         print(result)
         
     def update(self, UserId,New_Entercounter):
+        # Ubdating Firebase database
         self.Tirebase.put('/'+UserId+'/','EnterCounter',New_Entercounter)
     
-
-test=Web_Api('https://web-functionalies-api.firebaseio.com/')
-test.read('12345678','EnterCounter')
-test.update('12345678','123456','100')
-test.read('12345678','EnterCounter')
+# For testing purposes 
+#test=Web_Api('https://web-functionalies-api.firebaseio.com/')
+#test.read('12345678','EnterCounter')
+#test.update('12345678','123456','100')
+#test.read('12345678','EnterCounter')
     
     
     
